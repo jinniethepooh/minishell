08e849f9c1d00cf700ee16f6203da98e21d54bc4
jinniethepoohc <jinnie@apples-MacBook-Pro.local>
2022-09-04 14:07:24 +0700

    revised split/interpret var

--- srcs/mini_lexer_utils_4.c
+++ srcs/mini_lexer_utils_4.c
@@ -1,41 +1,69 @@
 #include "minishell.h"
 
-static void *cmd_cleaner_var(char *src)
+static void *cmd_var_gen(char *src, char *dst)
 {
-    char    *tmp;
-    char    *dst;
-    int     i;
+    char    *tmp1;
+    char    *tmp2;
+    int         i;
 
     i = 0;
-    if (*(src + 1) == '?')
+    tmp1 = ft_substr(&src[i + 1], 0, ft_loop_until(&src[i + 1], '$', 0));
+    tmp2 = mini_getenv(tmp1);
+    if (tmp2)
+        dst = ft_strjoin(dst, tmp2);
+    free(tmp1);
+    return (dst);
+}
+
+static void *cmd_var_exit(char *src, char *dst)
+{
+    char    *tmp;
+    int         i;
+
+    dst = ft_strjoin(dst, ft_itoa(g_var.exit_status));
+    i = 2;
+    if (src[i] != '$')
     {
-        dst = ft_itoa(g_var.exit_status);
-        if (ft_strlen(src + 2))
-            dst = ft_strjoin(dst, src + 2);
-        return (dst);
+        tmp = ft_substr(&src[i], 0, ft_loop_until(&src[i], '$', 0));
+        dst = ft_strjoin(dst, tmp);
+        free(tmp);
     }
-    tmp = ft_strjoin(src + 1, "=");
-    while (g_var.env[i] && ft_strncmp(tmp, g_var.env[i], ft_strlen(tmp)))
-        ++i;
-    if (!ft_strncmp(tmp, g_var.env[i], ft_strlen(tmp)))
-        dst = ft_substr(g_var.env[i], ft_strlen(tmp), ft_strlen(g_var.env[i]));
-    else
-        dst = ft_strdup("");
-    free(tmp);
+    return (dst);
+}
+
+static void *cmd_cleaner_var(char *src)
+{
+    int         i;
+    char    *dst;
+
+    i = 0;
+    dst = NULL;
+    while (src[i])
+    {
+        if (src[i + 1] == '?')
+        {
+            dst = cmd_var_exit(&src[i], dst);
+            i += (2 + ft_loop_until(&src[i], '$', 0));
+        }
+        else
+        {
+            dst = cmd_var_gen(&src[i], dst);
+            i += ft_loop_until(&src[i + 1], '$', 0) + 1;
+        }
+    }
     return (dst);
 }
 
 static void    *cmd_cleaner_loop(char *src)
 {
     if (ft_isquotes(*src))
-        printf("q\n");
+        return(ft_strdup(src));
     else if (ft_isredir(*src))
-        printf("r\n");
+        return(ft_strdup(src));
     else if (*src == '$' && ft_strcmp(src, "$"))
         return (cmd_cleaner_var(src));
     else
-        return(ft_strdup(src));
-    return (NULL);
+        return(ft_strdup(src)); // to add having $
 }
 
 char    **cmd_cleaner(char **arr)
