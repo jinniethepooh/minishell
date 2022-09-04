37c9acdc45cce68fa53c572c29d31af919e3422c
pradrattana <pradrattana@gmail.com>
2022-09-04 20:40:16 +0700

    fix leak in cmd_cleaner_loop

--- srcs/mini_cleaner_cmd.c
+++ srcs/mini_cleaner_cmd.c
@@ -1,59 +1,64 @@
 #include "minishell.h"
 
-// void    *cmd_cleaner_loop(char *src)
+// void	*cmd_cleaner_loop(char *src)
 // {
-//     if (ft_isquotes(*src))
-//         return(cmd_cleaner_quotes(src));
-//     else if (ft_isredir(*src))
-//         return(ft_strdup(src));
-//     else if (*src == '$' && ft_strcmp(src, "$"))
-//         return (cmd_cleaner_var(src));
-//     else
-//         return(ft_strdup(src)); // to add having $
+//	 if (ft_isquotes(*src))
+//		 return(cmd_cleaner_quotes(src));
+//	 else if (ft_isredir(*src))
+//		 return(ft_strdup(src));
+//	 else if (*src == '$' && ft_strcmp(src, "$"))
+//		 return (cmd_cleaner_var(src));
+//	 else
+//		 return(ft_strdup(src)); // to add having $
 // }
 // static  char *cmd_cleaner_gen(char *src)
 // {
-//     t_cmd_loop  g;
+//	 t_cmd_loop  g;
 
-//     g.i = 0;
-//     g.dst = ft_substr(src, 0, get_cmd_move(&src[g.i]));
+//	 g.i = 0;
+//	 g.dst = ft_substr(src, 0, get_cmd_move(&src[g.i]));
 // }
 
-void    *cmd_cleaner_loop(char *src)
+void	*cmd_cleaner_loop(char *src)
 {
-    char    *dst;
-    int         i;
+	char	*dst;
+	char	*tmp;
+	int		i;
 
-    dst = NULL;
-    i = 0;
-    while (src[i])
-    {
-        // else if (ft_isredir(src[i]))
-        //     i += ft_strlen(ft_strdup(&src[i]));
-        if (ft_isquotes(src[i]))
-            dst = ft_strjoin(dst, cmd_cleaner_quotes(&src[i]));
-        else if (src[i] == '$' && src[i + 1] && !ft_isspace(src[i + 1]))
-            dst = ft_strjoin(dst, cmd_cleaner_var(&src[i]));
-        else
-            dst = ft_strjoin(dst, ft_substr(&src[i], 0, get_cmd_move(&src[i])));
-        i += get_cmd_move(&src[i]);
-    }
-    return (dst);
+	dst = NULL;
+	i = 0;
+	//if (ft_strchr(src, '$'))
+	//	return (ft_strdup(src));
+	while (src[i])
+	{
+		// else if (ft_isredir(src[i]))
+		//	 i += ft_strlen(ft_strdup(&src[i]));
+		if (ft_isquotes(src[i]))
+			tmp = ft_strjoin(dst, cmd_cleaner_quotes(&src[i]));
+		else if (src[i] == '$' && src[i + 1] && !ft_isspace(src[i + 1]))
+			tmp = ft_strjoin(dst, cmd_cleaner_var(&src[i]));
+		else
+			tmp = ft_strjoin(dst, ft_substr(&src[i], 0, get_cmd_move(&src[i])));
+		free(dst);
+		dst = tmp;
+		i += get_cmd_move(&src[i]);
+	}
+	return (dst);
 }
 
-char    **cmd_cleaner(char **arr)
+char	**cmd_cleaner(char **arr)
 {
-    char    **clean;
-    int     i;
+	char	**clean;
+	int	 i;
 
-    i = 0;
-    clean = (char **)malloc((size_2d(arr) + 1) * sizeof(char *));
-    while (arr[i])
-    {
-        clean[i] = cmd_cleaner_loop(arr[i]);
-        ++i;
-    }
-    clean[i] = 0;
-    free_2d(arr);
-    return (clean);
+	i = 0;
+	clean = (char **)malloc((size_2d(arr) + 1) * sizeof(char *));
+	while (arr[i])
+	{
+		clean[i] = cmd_cleaner_loop(arr[i]);
+		++i;
+	}
+	clean[i] = 0;
+	free_2d(arr);
+	return (clean);
 }
